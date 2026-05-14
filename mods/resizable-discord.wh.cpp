d474a8253a8bd3cfe9387de4f319d1b8a99e5071
Gameknight963 <sebisebpaw@gmail.com>
2026-05-13 20:25:23 -0500

    Restore window maximize box handling via message hook

--- mods/resizable-discord.wh.cpp
+++ mods/resizable-discord.wh.cpp
@@ -2,7 +2,7 @@
 // @id              resizable-discord
 // @name            Discord Transparency Resize Fix
 // @description     Forces discord to be resizable with vencord window transparency
-// @version         1.0.1
+// @version         1.0.2
 // @author          gameknight963
 // @include         discord.exe
 // @github          https://github.com/Gameknight963
@@ -26,6 +26,9 @@ set WS_THICKFRAME, so the window is not resizable. This mod fixes that.
 typedef BOOL(WINAPI* ShowWindow_t)(HWND, int);
 ShowWindow_t ShowWindow_Original;
 
+typedef LRESULT(WINAPI* DispatchMessageW_t)(const MSG*);
+DispatchMessageW_t DispatchMessageW_Original;
+
 void HookWindow(HWND hWnd)
 {
     DWORD pid;
@@ -45,9 +48,20 @@ BOOL WINAPI ShowWindow_Hook(HWND hWnd, int nCmdShow) {
 
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM) { HookWindow(hWnd); return TRUE; }
 
+LRESULT WINAPI DispatchMessageW_Hook(const MSG* lpMsg)
+{
+    if (lpMsg->message == WM_NCLBUTTONDOWN && lpMsg->wParam == HTMAXBUTTON)
+    {
+        ShowWindow_Original(lpMsg->hwnd, IsZoomed(lpMsg->hwnd) ? SW_RESTORE : SW_MAXIMIZE);
+        return 0;
+    }
+    return DispatchMessageW_Original(lpMsg);
+}
+
 BOOL Wh_ModInit()
 {
     Wh_SetFunctionHook((void*)ShowWindow, (void*)ShowWindow_Hook, (void**)&ShowWindow_Original);
+    Wh_SetFunctionHook((void*)DispatchMessageW, (void*)DispatchMessageW_Hook, (void**)&DispatchMessageW_Original);
     EnumWindows(EnumWindowsProc, 0);
     return TRUE;
 }
